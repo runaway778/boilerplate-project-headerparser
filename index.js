049badd0cd77d4abd8d8e13e0495384a60e0800d
runaway778 <batyrov-amir@bk.ru>
2023-03-23 12:53:48 +0300

    Removed debug log

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.get('/api/hello', function (req, res) {
 });
 
 app.get('/api/whoami', (req, res) => {
-  console.log(req.headers);
   res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
 })
 
